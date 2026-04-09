4703ba51b5fe262d969f6d8919176a4f7084f801
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-04-09 11:21:03 +0300

    chore: add prettier to eslint

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,6 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 
-
 import { useModalStore } from "../stores/modalStore";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { useRecordStore } from "../stores/recordStore";
@@ -28,30 +27,41 @@ function Navbar() {
               display: "flex",
               height: "2.5rem",
               gap: ".5rem",
-              alignItems: "center",
+              alignItems: "space-between",
               width: "100%",
-
-              justifyContent: "flex-end",
             }}
           >
-            {records.length > 0 && (
-              <Button
-                variant="outlined"
-                color="white"
-                sx={{ py: 1, px: 4 }}
-                onClick={() => handleModalStore(true)}
-              >
-                <Typography fontSize={"1rem"}>{t("addNew")}</Typography>
-                <AddCircleOutlineIcon
-                  sx={{ fontSize: "1.5rem", marginInlineStart: 1 }}
-                />
-              </Button>
-            )}
-            <LanguageSwitcher />
+            <Box>his is a test</Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                height: "2.5rem",
+                gap: ".5rem",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              {records.length > 0 && (
+                <Button
+                  variant="outlined"
+                  color="white"
+                  sx={{ py: 1, px: 4 }}
+                  onClick={() => handleModalStore(true)}
+                >
+                  <Typography fontSize={"1rem"}>{t("addNew")}</Typography>
+                  <AddCircleOutlineIcon
+                    sx={{ fontSize: "1.5rem", marginInlineStart: 1 }}
+                  />
+                </Button>
+              )}
+              <LanguageSwitcher />
+            </Box>
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
+
 export default Navbar;
