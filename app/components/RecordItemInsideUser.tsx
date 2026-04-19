19b78f6fb87b4797a54d209c95c3dbb678503bf5
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-04-19 13:54:45 +0300

    chore: remove unused component

--- app/components/RecordItemInsideUser.tsx
+++
@@ -1,6 +0,0 @@
-//Used to display the records of a specific user inside the #RecordFormModal
-function RecordItemInsideUser() {
-  return <div>RecordItemInsideUser</div>;
-}
-
-export default RecordItemInsideUser;
