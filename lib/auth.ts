c005314cf0efe7e6a502b7cf2de5a9878ed98131
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-06-13 11:52:26 +0300

    fix(Auth): Database connection

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,9 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   database: new Pool({
     connectionString: process.env.DB_CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   }),
   socialProviders: {
     google: {
