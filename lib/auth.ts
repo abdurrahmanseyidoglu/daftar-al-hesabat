ac530bb5a2c1cb631f343e8265b43bee9e01af77
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-06-13 11:46:54 +0300

    feat(Auth): integrate PostgreSQL database connection

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,12 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
+import { Pool } from "pg";
+
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
+  database: new Pool({
+    connectionString: process.env.DB_CONNECTION_STRING,
+  }),
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
